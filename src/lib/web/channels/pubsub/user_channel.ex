71d7aafaa6117a52568eb6c0ecfc359d5e580964
Relrin <relrin78@gmail.com>
2024-01-03 18:08:08 +0100

    Implemented tests for internal pubsub events

--- src/lib/web/channels/pubsub/user_channel.ex
+++ src/lib/web/channels/pubsub/user_channel.ex
@@ -2,7 +2,7 @@ defmodule NotificationsServiceWeb.UserChannel do
   use Phoenix.Channel
   require Logger
 
-  intercept ["subscribe_internal", "unsubscribe_internal"]
+  intercept ["subscribe", "unsubscribe"]
 
   def join("user:" <> request_user_id, _payload, socket = %{assigns: %{user_id: user_id}}) do
     if request_user_id == to_string(user_id) do
@@ -13,36 +13,39 @@ defmodule NotificationsServiceWeb.UserChannel do
     end
   end
 
-  def handle_out("subscribe_internal", %{"topics" => topics}, socket) do
-    assign_new_topics(socket, topics)
-    {:noreply, socket}
+  def handle_out("subscribe", %{topics: topics}, socket) do
+    next_socket = assign_new_topics(socket, topics)
+
+    {:noreply, next_socket}
   end
 
-  def handle_out("unsubscribe_internal", %{"topics" => topics}, socket) do
-    remove_topics(socket, topics)
-    {:noreply, socket}
+  def handle_out("unsubscribe", %{topics: topics}, socket) do
+    next_socket = remove_topics(socket, topics)
+
+    {:noreply, next_socket}
   end
 
   defp assign_new_topics(socket, topics) do
     Enum.reduce(topics, socket, fn topic, acc ->
-      existing_topics = acc.assigns.topics
+      existing_topics = Map.get(acc.assigns, :topics, [])
       if topic in existing_topics do
         acc
       else
         :ok = NotificationsServiceWeb.Endpoint.subscribe(topic)
-        assign(acc, :topics, [topic | topics])
+        assign(acc, :topics, [topic | existing_topics])
       end
     end)
   end
 
   defp remove_topics(socket, topics) do
     Enum.reduce(topics, socket, fn topic, acc ->
-      existing_topics = acc.assigns.topics
+      existing_topics = Map.get(acc.assigns, :topics, [])
       if topic in existing_topics do
         :ok = NotificationsServiceWeb.Endpoint.unsubscribe(topic)
+        assign(acc, :topics, Enum.reject(existing_topics, fn topic_name -> topic_name == topic end))
+      else
+        acc
       end
-
-      acc
     end)
   end
 end
--- src/test/web/channels/pubsub/user_channel_test.exs
+++ src/test/web/channels/pubsub/user_channel_test.exs
@@ -19,4 +19,66 @@ defmodule NotificationsServiceWeb.UserChannelTest do
              |> subscribe_and_join("user:1", %{}) == {:error, %{reason: "unauthorized"}}
            end) =~ "[error] #{UserChannel} failed 1 != 2"
   end
+
+  test "subscribe_internal/3 assigns new topic to listen" do
+    topics = ["group:12345", "game"]
+    expected_topics = Enum.reverse(topics)
+
+    connect()
+    |> broadcast_subscribe_topics(topics)
+    |> validate_assigned_topics(expected_topics)
+  end
+
+  test "subscribe_internal/3 assigns new topic to listen and avoid duplicates" do
+    topics = ["group:12345", "game", "game"]
+    expected_topics = Enum.reverse(Enum.dedup(topics))
+
+    connect()
+    |> broadcast_subscribe_topics(topics)
+    |> validate_assigned_topics(expected_topics)
+  end
+
+  test "unsubscribe_internal/3 removes topics" do
+    topics = ["group:12345", "game"]
+    expected_topics = Enum.reverse(topics)
+
+    connect()
+    |> broadcast_subscribe_topics(topics)
+    |> validate_assigned_topics(expected_topics)
+    |> broadcast_unsubscribe_topics(topics)
+    |> validate_assigned_topics([])
+  end
+
+  test "unsubscribe_internal/3 removes topics (and excluding duplicates)" do
+    topics = ["group:12345", "game", "game"]
+    expected_topics = Enum.reverse(Enum.dedup(topics))
+
+    connect()
+    |> broadcast_subscribe_topics(topics)
+    |> validate_assigned_topics(expected_topics)
+    |> broadcast_unsubscribe_topics(topics)
+    |> validate_assigned_topics([])
+  end
+
+  defp connect() do
+    assert {:ok, _, socket} =
+             socket(PubSubSocket, "pubsub_socket:1", %{user_id: 1})
+             |> subscribe_and_join("user:1", %{})
+    socket
+  end
+
+  defp broadcast_subscribe_topics(socket, topics) do
+    assert broadcast_from!(socket, "subscribe", %{topics: topics}) == :ok
+    socket
+  end
+
+  defp broadcast_unsubscribe_topics(socket, topics) do
+    assert broadcast_from!(socket, "unsubscribe", %{topics: topics}) == :ok
+    socket
+  end
+
+  defp validate_assigned_topics(socket, expected_topics) do
+    assert :sys.get_state(socket.channel_pid).assigns.topics == expected_topics
+    socket
+  end
 end
