bda711c58988c727e54215adddfe3dfb2a8972f2
Relrin <relrin78@gmail.com>
2024-01-03 16:21:20 +0100

    Added internal subscribe/unsubscribe message interceptors

--- src/lib/web/channels/pubsub/user_channel.ex
+++ src/lib/web/channels/pubsub/user_channel.ex
@@ -2,6 +2,8 @@ defmodule NotificationsServiceWeb.UserChannel do
   use Phoenix.Channel
   require Logger
 
+  intercept ["subscribe_internal", "unsubscribe_internal"]
+
   def join("user:" <> request_user_id, _payload, socket = %{assigns: %{user_id: user_id}}) do
     if request_user_id == to_string(user_id) do
       {:ok, socket}
@@ -9,5 +11,38 @@ defmodule NotificationsServiceWeb.UserChannel do
       Logger.error("#{__MODULE__} failed #{request_user_id} != #{user_id}")
       {:error, %{reason: "unauthorized"}}
     end
+  end
+
+  def handle_out("subscribe_internal", %{"topics" => topics}, socket) do
+    assign_new_topics(socket, topics)
+    {:noreply, socket}
+  end
+
+  def handle_out("unsubscribe_internal", %{"topics" => topics}, socket) do
+    remove_topics(socket, topics)
+    {:noreply, socket}
+  end
+
+  defp assign_new_topics(socket, topics) do
+    Enum.reduce(topics, socket, fn topic, acc ->
+      existing_topics = acc.assigns.topics
+      if topic in existing_topics do
+        acc
+      else
+        :ok = NotificationsServiceWeb.Endpoint.subscribe(topic)
+        assign(acc, :topics, [topic | topics])
+      end
+    end)
+  end
+
+  defp remove_topics(socket, topics) do
+    Enum.reduce(topics, socket, fn topic, acc ->
+      existing_topics = acc.assigns.topics
+      if topic in existing_topics do
+        :ok = NotificationsServiceWeb.Endpoint.unsubscribe(topic)
+      end
+
+      acc
+    end)
   end
 end
