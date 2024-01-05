964202a6ce1d87a7da9fb30cef9b98f64462e1c8
Relrin <relrin78@gmail.com>
2024-01-05 11:37:43 +0100

    Added PubSub definition for testing

--- src/lib/app/application.ex
+++ src/lib/app/application.ex
@@ -11,14 +11,7 @@ defmodule NotificationsService.Application do
       NotificationsServiceWeb.Telemetry,
       {GRPC.Server.Supervisor, endpoint: NotificationsServiceGrpc.Endpoint, port: 8080, start_server: true},
       {DNSCluster, query: Application.get_env(:notifications_service, :dns_cluster_query) || :ignore},
-      {Phoenix.PubSub,
-        adapter: Phoenix.PubSub.Redis,
-        name: NotificationsService.PubSub,
-        node_name: System.get_env("NODE", "NotificationsService"),
-        host: System.get_env("REDIS_HOST", "redis"),
-        #password: System.get_env("REDIS_PORT", ""),
-        redis_pool_size: 10,
-      },
+      get_pubsub_definition(),
       # Start a worker by calling: NotificationsService.Worker.start_link(arg)
       # {NotificationsService.Worker, arg},
       # Start to serve requests, typically the last entry
@@ -37,5 +30,23 @@ defmodule NotificationsService.Application do
   def config_change(changed, _new, removed) do
     NotificationsServiceWeb.Endpoint.config_change(changed, removed)
     :ok
+  end
+
+  defp get_pubsub_definition() do
+    if Mix.env == :prod do
+      {Phoenix.PubSub,
+        adapter: Phoenix.PubSub.Redis,
+        name: NotificationsService.PubSub,
+        node_name: System.get_env("NODE", "NotificationsService"),
+        host: System.get_env("REDIS_HOST", "redis"),
+        password: System.get_env("REDIS_PORT", ""),
+        redis_pool_size: 10,
+      }
+    else
+      {Phoenix.PubSub,
+        adapter: Phoenix.PubSub.PG2,
+        name: NotificationsService.PubSub
+      }
+    end
   end
 end
