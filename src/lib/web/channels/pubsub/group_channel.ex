3ac57135d7f002855bd8980ef03d9ccf7026994d
Relrin <relrin78@gmail.com>
2024-01-08 19:22:58 +0100

    Added tests for the GroupChannel

--- src/lib/web/channels/pubsub/group_channel.ex
+++ src/lib/web/channels/pubsub/group_channel.ex
@@ -2,10 +2,14 @@ defmodule NotificationsServiceWeb.GroupChannel do
   use Phoenix.Channel
   require Logger
 
-  def join("group:" <> request_group_id, _payload, _socket = %{assigns: %{user_id: user_id}}) do
-    Logger.error("#{__MODULE__} User #{user_id} tried to join the group:#{request_group_id} channel")
-    {:error, %{reason: "unauthorized"}}
+  alias Phoenix.Socket.Broadcast
+
+  def join("group:" <> _request_group_id, _payload, socket) do
+    {:ok, socket}
   end
 
-
+  def handle_info(%Broadcast{topic: _, event: event, payload: payload}, socket) do
+    push(socket, event, payload)
+    {:noreply, socket}
+  end
 end
--- src/test/web/channels/pubsub/group_channel_test.exs
+++ src/test/web/channels/pubsub/group_channel_test.exs
@@ -1,16 +1,45 @@
 defmodule NotificationsServiceWeb.GroupChannelTest do
   use NotificationsServiceWeb.ChannelCase
-  import ExUnit.CaptureLog
 
-  alias NotificationsServiceWeb.{
-    PubSubSocket,
-    GroupChannel,
-  }
+  alias NotificationsServiceWeb.PubSubSocket
 
-  test "join/3 failed for a user accessing private channel" do
-    assert capture_log(fn ->
+  test "join/3 is successful" do
+    assert {:ok, _, %Phoenix.Socket{}} =
              socket(PubSubSocket, "pubsub_socket:1", %{user_id: 1})
-             |> subscribe_and_join("group:private_group", %{}) == {:error, %{reason: "unauthorized"}}
-           end) =~ "[error] #{GroupChannel}"
+             |> subscribe_and_join("group:123", %{})
+  end
+
+  test "join/3 is successful (including user)" do
+    assert {:ok, _, pubsub_socket} =
+             socket(PubSubSocket, "pubsub_socket:1", %{user_id: 1})
+             |> subscribe_and_join("user:1", %{})
+
+    assert {:ok, _, _} = subscribe_and_join(pubsub_socket, "group:123", %{})
+  end
+
+  test "handle_info/2 is triggerred for the group broadcast" do
+    connect()
+    |> broadcast_message("update", %{key: "value"})
+
+    assert {:messages,
+      [
+        %Phoenix.Socket.Message{
+          topic: "group:123",
+          event: "update",
+          payload: %{key: "value"}
+        },
+      ]} = Process.info(self(), :messages)
+  end
+
+  defp connect() do
+    assert {:ok, _, socket} =
+             socket(PubSubSocket, "pubsub_socket:1", %{user_id: 1})
+             |> subscribe_and_join("group:123", %{})
+    socket
+  end
+
+  defp broadcast_message(socket, event, payload) do
+    assert broadcast_from!(socket, event, payload) == :ok
+    socket
   end
 end
